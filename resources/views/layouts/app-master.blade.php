9ddc8131bf8c6414d35b323a1cffe3e43b371385
DavidUhlar <dadododo02@gmail.com>
2023-11-13 15:26:35 +0100

    pondelok 13.11.2023

--- resources/views/layouts/app-master.blade.php
+++ resources/views/layouts/app-master.blade.php
@@ -36,9 +36,9 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <link rel="stylesheet" href=" {{ asset("css/style.css")}}">
-        <link rel="stylesheet" href="css/womac.css">
-        <link rel="stylesheet" href="css/navbar.css">
-        <link rel="stylesheet" href="css/mapa.css">
+        <link rel="stylesheet" href=" {{ asset("css/womac.css")}}">
+        <link rel="stylesheet" href=" {{ asset("css/navbar.css")}}">
+        <link rel="stylesheet" href=" {{ asset("css/mapa.css")}}">
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
